1df048cd1a7359f7d5b04fee68b9224a73658e96
Ege Hurturk <ege.hurturk@gmail.com>
2021-04-10 17:23:43 +0300

    Added new tests

--- src/test/java/com/easycli/EasyCliTest.java
+++ src/test/java/com/easycli/EasyCliTest.java
@@ -1,7 +1,6 @@
 package com.easycli;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,21 +11,173 @@ import org.junit.jupiter.api.Test;
 public class EasyCliTest {
 
     @Test
-    @DisplayName("EasyCli tests")
-    void commandLineValueShouldBeAccessedTwo() {
-        // ./banzai -c|--config file_path [-v|--version] [-o|--verbose]
-        String[] args = {"./banzai", "-c", "/dev/null", "-v", "-o"};
-        EasyCli cli = new EasyCli(args, getOptions());
-        if (!cli.matchAllArgs()) {
-            EasyCli.Synopsis synopsis = new EasyCli.Synopsis();
-            synopsis.print();
-            System.exit(cli.getStatus());
-        }
-        System.out.println(cli.has("foo")); // false
-        String val = cli.get("file_path"); // should give the value of file_path
-        Assertions.assertEquals("/dev/null", val);
+    @DisplayName("Description longer than 200 should give error")
+    void testDescriptionLong() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Arg.with()
+                .longOptionName("file")
+                .shortOptionName("f")
+                .argName("file_path")
+                .description("This represents the file pathalsfjsdalgjs;dlfkgjs;lfkgjs;kldfj;lsdkfjgs;dlkfgjs;flkjk" +
+                        "salfjasdkfja;dslkfja;ka;dslkfja;dslkfjads;lkfjadsflasfpoiauewtoiuwerpotuweprituwpeitupweirutpwio" +
+                        "erwtuweroptiuewrpoituwperoituwporitpwoeritupwoireutpwoierutpoiwerutpowirutpowirutpowierutpowierutp" +
+                        "woeriutewproituweprtiuwepritupweroitupweritupweirupowireutpiwerut")
+                .required()
+                .alias("config")
+                .build());
+        Assertions.assertThrows(IllegalArgumentException.class, () ->Flag.with()
+                .longOptionName("debug")
+                .shortOptionName("d")
+                .description("Enable debugreotiuwerpoituwpeoritupweoritupweoritupeiowrutpweoirtupoweirutwpoeirutpeiru" +
+                        "weropituweproitupweoritupweroitupweoirtupweoriutpwoeirutpoweirutpwerit" +
+                        "weroituwerpotiuweproitupwoirtupwoerutpweoritupweoritupwoeritupeowritpweoirut")
+                .optional()
+                .alias("verbose")
+                .build());
 
     }
+
+    @Test
+    @DisplayName("Test Synopsis 5")
+    void testSynopsisAgainAndAgain() {
+        String[] args = {"./banzai"};
+        EasyCli cli = new EasyCli(args, getLongOptions());
+        if (!cli.matchAllArgs()) {
+            EasyCli.Synopsis helper = cli.new Synopsis("euroleague");
+            helper.print();
+        }
+    }
+
+    private CmdOptions getLongOptions() {
+        Arg config = Arg.with()
+                .longOptionName("file")
+                .shortOptionName("f")
+                .argName("file_path")
+                .description("This represents the file pathasdlfjasdlkfjas;dlkfjas;kdfjas;dkfj;alksdfiower")
+                .required()
+                .alias("config")
+                .build();
+        Flag ver = Flag.with()
+                .longOptionName("debug")
+                .shortOptionName("d")
+                .description("Enable debugrwqoieruowqierupqoeirupowieupoqwie")
+                .optional()
+                .alias("verbose")
+                .build();
+        return new CmdOptions(config, ver);
+    }
+
+
+
+    @Test
+    @DisplayName("Easy client should not throw anything if we have nonnull args and objects")
+    void testEasyClientConstructorWithNonNull() {
+        String[] args = {"./banzai", "-c", "/dev/null", "-v", "-o"};
+        EasyCli cli = new EasyCli(args, getOptions());
+    }
+
+    @Test
+    @DisplayName("Easy client should throw nullpointerexception if we have null args or objects")
+    void testEasyClientConstructorWithNull() {
+        String[] args = {"./banzai", "-c", "/dev/null", "-v", "-o"};
+        Assertions.assertThrows(NullPointerException.class, () ->  new EasyCli(args, null));
+        Assertions.assertThrows(NullPointerException.class, () -> new EasyCli(null, getOptions()));
+        Assertions.assertThrows(NullPointerException.class, () -> new EasyCli(null, null));
+    }
+
+    @Test
+    @DisplayName("Easy client should return false for required and non existent options")
+    void testEasyClientMatchArgs() {
+        String[] args = {"./banzai", "-c", "/dev/null", "-v", "-o"};
+        String[] args2 = {"./banzai", "-v", "-o"};
+        String[] args3 = {"./banzai"};
+        String[] args4 = {"./banzai", "-o"};
+        EasyCli cli = new EasyCli(args, getOptions());
+        EasyCli cli2 = new EasyCli(args2, getOptions());
+        EasyCli cli3 = new EasyCli(args3, getOptions());
+        EasyCli cli4 = new EasyCli(args4, getOptions());
+        Assertions.assertTrue(cli.matchAllArgs());
+        Assertions.assertFalse(cli2.matchAllArgs());
+        Assertions.assertFalse(cli3.matchAllArgs());
+        Assertions.assertFalse(cli4.matchAllArgs());
+    }
+
+    @Test
+    @DisplayName("Test Synopsis")
+    void testSynopsis() {
+        String[] args = {"./banzai", "/dev/null", "-v", "-o"};
+        EasyCli cli = new EasyCli(args, getOptions());
+        if (!cli.matchAllArgs()) {
+            EasyCli.Synopsis helper = cli.new Synopsis("euroleague");
+            String helpMessage = helper.getHelpMessage();
+            String j = "Usage: euroleague -c|--config <file path> [-v|--version] [-o|--verbose] [-h|--help]\n" +
+                    "\t-c|--config <file path>      Configuration file path\n" +
+                    "\t-v|--version                 Get version\n" +
+                    "\t-o|--verbose                 Get version\n" +
+                    "\t-h|--help                    Print this message\n";
+            Assertions.assertEquals(helpMessage, j);
+        }
+    }
+
+    @Test
+    @DisplayName("Test Synopsis Again")
+    void testSynopsisAgain() {
+        String[] args = {"/dev/Null", "-h"};
+        EasyCli cli = new EasyCli(args, getOptions4());
+        if (!cli.matchAllArgs()) {
+            EasyCli.Synopsis synop = cli.new Synopsis("<filename>");
+            synop.print();
+        }
+    }
+
+    private CmdOptions getOptions4() {
+        Arg config = Arg.with()
+                .longOptionName("file")
+                .shortOptionName("f")
+                .argName("file_path")
+                .description("This represents the file path")
+                .required()
+                .alias("config")
+                .build();
+        Flag ver = Flag.with()
+                .longOptionName("debug")
+                .shortOptionName("d")
+                .description("Enable debug")
+                .optional()
+                .alias("verbose")
+                .build();
+        return new CmdOptions(config, ver);
+    }
+
+    @Test
+    @DisplayName("Test has method for EasyCli")
+    void testHasMethod() {
+        String[] args = {"--file", "/dev/null", "-d"};
+        EasyCli cli = new EasyCli(args, getOptions4());
+        if (!cli.matchAllArgs()) {
+            EasyCli.Synopsis synopsis = cli.new Synopsis("easport");
+            synopsis.print();
+        }
+        Assertions.assertTrue(cli.has("config"));
+        Assertions.assertTrue(cli.has("verbose"));
+        Assertions.assertFalse(cli.has("sldkfjsl;dkj"));
+        Assertions.assertFalse(cli.has("skjfhalskjfhalsdkjfhl"));
+        Assertions.assertFalse(cli.has("file"));
+    }
+
+    @Test
+    @DisplayName("Test get method")
+    void testGetMethod() {
+        String[] args = {"--file", "/dev/null", "-d"};
+        EasyCli cli = new EasyCli(args, getOptions4());
+        if (!cli.matchAllArgs()) {
+            EasyCli.Synopsis synopsis = cli.new Synopsis("easport");
+            synopsis.print();
+        }
+        Assertions.assertThrows(IllegalArgumentException.class, () -> cli.get("verbose"));
+        Assertions.assertEquals("/dev/null", cli.get("config"));
+        Assertions.assertNull(cli.get("asdfaskdjas"));
+    }
+
 
 
     @Test
@@ -43,29 +194,11 @@ public class EasyCliTest {
     @Test
     @DisplayName("If there are duplicate options, do not add the newer one")
     void checkDuplicateOptionsCmdOptionsClass() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> getDuplicateOptions());
+        Assertions.assertThrows(IllegalArgumentException.class, this::getDuplicateOptions);
         Assertions.assertThrows(IllegalArgumentException.class, this::getDuplicateOptions2);
     }
 
 
-    @Test
-    @Disabled
-    @DisplayName("Given command line arguments in array, program should make arguments accessible")
-    void commandLineValueShouldBeAccessed() {
-        // ./banzai -c|--config file_path [-v|--version] [-o|--verbose]
-        String[] args = {"./banzai", "-c", "/dev/null", "-v", "-o"};
-        EasyCli cli = new EasyCli(args, null);
-        if (!cli.isSuccessful()) {
-            EasyCli.Synopsis synopsis = cli.new Synopsis();
-            synopsis.print();
-            System.exit(cli.getStatus());
-        }
-
-        System.out.println(cli.has("foo"));
-        if (cli.has("bar")) {
-            System.out.println("This is bar boi: " + cli.valueOf("bar"));
-        }
-    }
 
     @Test
     @DisplayName("Build an argument and check its fields")
@@ -195,6 +328,7 @@ public class EasyCliTest {
                 .argName("file path")
                 .description("Configuration file path")
                 .required()
+                .alias("configuration")
                 .build();
 
         Flag ver = Flag.with()
@@ -202,6 +336,7 @@ public class EasyCliTest {
                 .shortOptionName("v")
                 .description("Get version")
                 .optional()
+                .alias("version")
                 .build();
 
         Flag verbose = Flag.with()
@@ -209,10 +344,10 @@ public class EasyCliTest {
                 .shortOptionName("o")
                 .description("Get version")
                 .optional()
+                .alias("verbose")
                 .build();
 
         CmdOptions options = new CmdOptions(config, ver, verbose);
-        System.out.println(options);
         return options;
     }
 
@@ -249,7 +384,6 @@ public class EasyCliTest {
                 .build();
 
         CmdOptions options = new CmdOptions(config, config2, ver, verbose);
-        System.out.println(options);
         return options;
     }
 
@@ -260,6 +394,7 @@ public class EasyCliTest {
                 .shortOptionName("c")
                 .argName("file path")
                 .description("Configuration file path")
+                .alias("configuration")
                 .required()
                 .build();
 
@@ -269,6 +404,7 @@ public class EasyCliTest {
                 .argName("fsdflgsd")
                 .description("sdflgsdjsdf;")
                 .required()
+                .alias("configuration2")
                 .build();
 
         Flag ver = Flag.with()
@@ -276,6 +412,7 @@ public class EasyCliTest {
                 .shortOptionName("v")
                 .description("Get version")
                 .optional()
+                .alias("config")
                 .build();
 
         Flag ver2 = Flag.with()
@@ -283,6 +420,7 @@ public class EasyCliTest {
                 .shortOptionName("vava")
                 .description("Get version")
                 .optional()
+                .alias("asdasdasd")
                 .build();
 
         Flag verbose = Flag.with()
@@ -290,10 +428,10 @@ public class EasyCliTest {
                 .shortOptionName("o")
                 .description("Get version")
                 .optional()
+                .alias("verbose")
                 .build();
 
         CmdOptions options = new CmdOptions(config, config2, ver, ver2, verbose);
-        System.out.println(options);
         return options;
     }
 
@@ -302,3 +440,5 @@ public class EasyCliTest {
 
 //https://stackoverflow.com/questions/9725675/is-there-a-standard-format-for-command-line-shell-help-text
 //http://www.tfug.org/helpdesk/general/man.html
+
+// TODO: limit description to 100 characters
